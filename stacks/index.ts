86dcddb1adecd7827bbcad1d43a8c09f5dd52fb8
Louis Latreille <louis_latreille@hotmail.com>
2022-10-25 22:00:24 +0000

    Add stack tags

--- stacks/index.ts
+++ stacks/index.ts
@@ -4,6 +4,7 @@ import * as cdk from "aws-cdk-lib";
 import { TodoRenameStack } from "./todoRename";
 import { DeploymentDefinition } from "./deploymentDefinition";
 import { ResourceNameGenerator } from "./resourceNameGenerator";
+import { Tags } from "aws-cdk-lib";
 
 const app = new cdk.App();
 
@@ -12,7 +13,9 @@ const deploymentDefinition = new DeploymentDefinition();
 // It is best to keep the stack name short (10 characters or less), as resource names have a limited number of characters
 ResourceNameGenerator.init("TodoRename", deploymentDefinition.getStage(), deploymentDefinition.getRegion());
 
-new TodoRenameStack(app, "TodoRenameStack", {
+const stack = new TodoRenameStack(app, "TodoRenameStack", {
   env: { account: deploymentDefinition.getAccountNumber(), region: deploymentDefinition.getRegion(), stage: deploymentDefinition.getStage() },
   stackName: ResourceNameGenerator.singleton().generateNameFor("stack"),
 });
+
+Tags.of(stack).add("Stage", deploymentDefinition.getStage());
